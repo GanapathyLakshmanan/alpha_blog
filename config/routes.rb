be82474cfcb7a0db388a7ee50857c1dc85b04cbe
Ganapathy <ganapathyror@gmail.com>
2020-05-11 03:02:55 +0530

    changes to UI by Adding Layout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	root 'pages#home'
+	get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  	resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
